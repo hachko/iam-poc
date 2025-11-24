cc975b8882366b498b4099d7e8e9ceae28be5cdf
jaknichan <jaknichan@yahoo.fr>
2025-11-24 09:46:31 +0100

    Issue #2 : set app main structure and routes to user admin page

---
+++ front-end/src/app/domains/shared/modal-host/modal-host.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-host',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-host.component.html',
+  styleUrl: './modal-host.component.css'
+})
+export class ModalHostComponent {
+
+}
---
+++ front-end/src/app/domains/users/components/users-admin-page/users-admin-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-admin-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-admin-page.component.html',
+  styleUrl: './users-admin-page.component.css'
+})
+export class UsersAdminPageComponent {
+
+}
